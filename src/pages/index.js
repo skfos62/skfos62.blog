f446a903fbac6b237c33eec76d162306e91b1c81
skfos62 <almighty62@naver.com>
2022-03-15 02:11:10 +0900

    etc 내용 추가 및 css 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
       <Layout pageTitle="Home">
         <div className={homeInfoContainer}>
           <div>
-            <p>안녕하세요. 디자인 경력이 있는 클라이언트 개발자 정나래 입니다. 👋 <br />저는 약 4년간의 웹디자이너 경력이 있습니다. <br /> 디자이너로써 일을 하면서 html, css 유지보수를 맡아서 진행했습니다. <br />간단하게나마 코딩을 접하고나니 코딩의 매력에 푹 빠져버려서 무작정 개발자가 되고싶었습니다. </p>
+            <p>안녕하세요.<br /> 디자인 경력이 있는 클라이언트 개발자 정나래 입니다. 👋 <br />저는 약 4년간의 웹디자이너 경력이 있습니다. <br /> 디자이너로써 일을 하면서 html, css 유지보수를 맡아서 진행했습니다. <br />간단하게나마 코딩을 접하고나니 코딩의 매력에 푹 빠져버려서 무작정 개발자가 되고싶었습니다. </p>
             <br />
             <p>클라이언트 개발을 경험하면서 개발자도 사용자들의 경험 개선에 큰 역할을 하고있는것을 깨달았습니다.</p>
             <p>웹과 어플리케이션에 대해 고민을 하며 사용자 경험을 개선하고 싶은 개발자 입니다.</p>
@@ -31,7 +31,7 @@ const IndexPage = () => {
           </Link>
               <p>2021 .03 .22 ~ 현재</p>
             </div>
-            <p style={{ marginLeft: 20 }}> - Frontend </p>
+            <div style={{ marginLeft: 20 }}> - Frontend </div>
           </div>
           <div style={{ marginTop: 10, }}>
             <div style={{ display: "flex", gap: 20, marginBottom: 10 }}>
@@ -40,7 +40,7 @@ const IndexPage = () => {
               </Link>
               <p>2015 .04 .01 ~ 2020. 06. 14</p>
             </div>
-            <p style={{ marginLeft: 20 }}> - Web Designer </p>
+            <div style={{ marginLeft: 20 }}> - Web Designer </div>
           </div>
         </div>
 
@@ -49,12 +49,12 @@ const IndexPage = () => {
           <h2 className={title}>Education</h2>
           <div>
             <div style={{ display: "flex", gap: 20, marginBottom: 10 }}>
-              <p>서울 디지털 사이버 대학 / 디자인과 졸업 </p>
+              <div>서울 디지털 사이버 대학 / 디자인과 졸업 </div>
             </div>
           </div>
           <div>
             <div style={{ display: "flex", gap: 20, marginBottom: 10 }}>
-              <p>영남 이공 대학 / 시각 디자인과 졸업 </p>
+              <div>영남 이공 대학 / 시각 디자인과 졸업 </div>
             </div>
           </div>
         </div>
@@ -64,34 +64,34 @@ const IndexPage = () => {
           <h2 className={title}>Project & Used Skils</h2>
           <div>
             <div style={{ display: "flex", gap: 20, marginBottom: 10, flexWrap: "wrap" }}>
-              <Link to="https://www.iclickart.co.kr/" className={careerLink}>
+              <Link to="https://portal.isyncbrain.com/auth/login" className={careerLink}>
                 IsyncBrain 3.0
               </Link>
-              <p>사이트 & 어드민 사이트 구축 및 유지보수 업무를 담당하였습니다.</p>
+              <div>사이트 & 어드민 사이트 구축 및 유지보수 업무를 담당하였습니다.</div>
             </div>
             <div>
               <div style={{ display: "flex", gap: 10, marginBottom: 10, flexDirection: "column", marginLeft: 20 }}>
-                <p>   - Typescript </p>
-                <p>   - React </p>
-                <p>   - Redux-saga </p>
-                <p>   - Redux-thunk </p>
-                <p>   - React-query </p>
+                <div>   - Typescript </div>
+                <div>   - React </div>
+                <div>   - Redux-saga </div>
+                <div>   - Redux-thunk </div>
+                <div>   - React-query </div>
               </div>
             </div>
           </div>
           <br />
           <div>
             <div style={{ display: "flex", gap: 20, marginBottom: 10, flexWrap: "wrap"}}>
-              <p style={{fontSize: 18}}>
+              <div style={{fontSize: 18}}>
                 IsyncBeat
-              </p>
-              <p>입사직후 ios 어플리케이션 구축 업무를 담당하였으나, IsyncBrain업무 투입 후 중단 </p>
+              </div>
+              <div>입사직후 ios 어플리케이션 구축 업무를 담당하였으나, IsyncBrain업무 투입 후 중단 </div>
             </div>
             <div>
               <div style={{ display: "flex", gap: 10, marginBottom: 10, flexDirection: "column", marginLeft: 20 }}>
-                <p>   - Swift </p>
-                <p>   - Swift Ui </p>
-                <p>   - Firebase </p>
+                <div>   - Swift </div>
+                <div>   - Swift Ui </div>
+                <div>   - Firebase </div>
               </div>
             </div>
           </div>
